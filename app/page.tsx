7e6a81e206129fcdd339e7d620f547891cb44fdf
NSULO JUSTIN <justinpunk50@gmail.com>
2026-05-21 12:33:38 +0200

    Redirect home page to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import { supabase } from "@/lib/supabase";
+import { redirect } from "next/navigation";
 
 export default function Home() {
 
-  useEffect(() => {
-    async function testConnection() {
-      const { data, error } = await supabase
-        .from("test")
-        .select("*");
+  redirect("/login");
 
-      console.log("DATA:", data);
-      console.log("ERROR:", error);
-    }
-
-    testConnection();
-  }, []);
-
-  return (
-    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-10 rounded-xl shadow-lg text-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-          Supabase Connected
-        </h1>
-
-        <p className="text-gray-600">
-          Project Management System
-        </p>
-      </div>
-    </main>
-  );
 }
